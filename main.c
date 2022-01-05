e765a6504df6782aa2ab7be9ec78750c30a33754
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2022-01-05 22:52:36 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,28 +4,34 @@ int main()
 {
 	pnode head = NULL;
 	char select;
-	while (scanf("%c", &select) !=EOF) {
-	    if(select=='A'){
+	scanf("%c", &select) ;
+	while (select!=EOF) {
+		switch (select)
+		{
+		case 'A': {
 			build_graph_cmd(&head);
-		}
-		if(select=='B'){
-			insert_node_cmd(&head);
-		  }
-		if(select=='D'){
-			delete_node_cmd(&head);
-	
-		}
-		if(select=='S'){
-			shortsPath_cmd(head);
-		}
-	
-		if(select=='T'){
-			TSP_cmd(head);
-		}
-		else{
 			break;
 		}
-		
+		case'B': {
+			insert_node_cmd(&head);
+			break;
+		}
+		case 'D': {
+			delete_node_cmd(&head);
+			break;
+		}
+		case'S':
+		{
+			shortsPath_cmd(head);
+			break;
+		}
+		case 'T': {
+			TSP_cmd(head);
+			break;
+		}
+
+		}
+     scanf("%c", &select);
 	}
 	//printGraph_cmd(head);
 	deleteGraph_cmd(&head);
